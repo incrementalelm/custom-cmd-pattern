956c178f85914420665feb4030cf811e28129d03
Dillon Kearns <dillon@dillonkearns.com>
2019-03-12 17:28:22 -0700

    Add example of custom http Cmd.

--- src/Main.elm
+++ src/Main.elm
@@ -1,66 +1,71 @@
 module Main exposing (main)
 
 import Browser
-import Html exposing (Html, button, div, h1, p, text)
-import Html.Events exposing (onClick)
-import Url
-import Url.Parser exposing ((</>))
+import Html exposing (Html, pre, text)
+import Http
 
 
-type alias Model =
-    Maybe String
+type MyCmd
+    = MyHttp (Result Http.Error String -> Msg) String
 
 
-type Msg
-    = OnUrlRequest Browser.UrlRequest
-    | OnUrlChange Url.Url
-
-
-init () url navigationKey =
-    ( parseUrl url, Cmd.none )
-
-
-parseUrl : Url.Url -> Maybe String
-parseUrl url =
-    url
-        |> Url.Parser.parse (Url.Parser.s "article" </> Url.Parser.string)
-
-
-type alias Article =
-    { title : String
-    , body : String
-    }
-
-
-view : Maybe String -> Browser.Document msg
-view maybeArticlePath =
-    articleNotFoundDocument
-
-
-articleDocument : Article -> Browser.Document msg
-articleDocument article =
-    { title = article.title
-    , body =
-        [ h1 [] [ text <| "Article - " ++ article.title ]
-        , p [] [ text article.body ]
-        ]
-    }
-
-
-articleNotFoundDocument : Browser.Document msg
-articleNotFoundDocument =
-    { title = "Article Not Found"
-    , body = [ text "Article not found..." ]
-    }
+myCmdToCmd : MyCmd -> Cmd Msg
+myCmdToCmd myCmd =
+    case myCmd of
+        MyHttp toMsg url ->
+            Http.get
+                { url = url
+                , expect = Http.expectString toMsg
+                }
 
 
 main : Program () Model Msg
 main =
-    Browser.application
-        { init = init
-        , view = view
-        , update = \msg model -> ( model, Cmd.none )
+    Browser.element
+        { init = \flags -> flags |> init |> Tuple.mapSecond myCmdToCmd
+        , update = update
         , subscriptions = \model -> Sub.none
-        , onUrlRequest = OnUrlRequest
-        , onUrlChange = OnUrlChange
+        , view = view
         }
+
+
+type Model
+    = Failure
+    | Loading
+    | Success String
+
+
+init : () -> ( Model, MyCmd )
+init _ =
+    ( Loading
+    , MyHttp GotText "https://elm-lang.org/assets/public-opinion.txt"
+    )
+
+
+type Msg
+    = GotText (Result Http.Error String)
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        GotText result ->
+            case result of
+                Ok fullText ->
+                    ( Success fullText, Cmd.none )
+
+                Err _ ->
+                    ( Failure, Cmd.none )
+
+
+view : Model -> Html Msg
+view model =
+    case model of
+        Failure ->
+            text "I was unable to load your book."
+
+        Loading ->
+            text "Loading..."
+
+        Success fullText ->
+            pre [] [ text fullText ]
